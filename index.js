ecf28b8710aacec5c6f3b4648a387fc631ff7132
Sunny <sun.ss.6767@gmail.com>
2024-06-01 20:49:31 +0530

    added my portfolio

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 // Load environment variables from .env file
 dotenv.config();
@@ -7,41 +9,14 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-const getGithubData = {
-  login: "SunnySinghSaini",
-  id: 31573844,
-  node_id: "MDQ6VXNlcjMxNTczODQ0",
-  avatar_url: "https://avatars.githubusercontent.com/u/31573844?v=4",
-  gravatar_id: "",
-  url: "https://api.github.com/users/SunnySinghSaini",
-  html_url: "https://github.com/SunnySinghSaini",
-  followers_url: "https://api.github.com/users/SunnySinghSaini/followers",
-  following_url: "https://api.github.com/users/SunnySinghSaini/following{/other_user}",
-  gists_url: "https://api.github.com/users/SunnySinghSaini/gists{/gist_id}",
-  starred_url: "https://api.github.com/users/SunnySinghSaini/starred{/owner}{/repo}",
-  subscriptions_url: "https://api.github.com/users/SunnySinghSaini/subscriptions",
-  organizations_url: "https://api.github.com/users/SunnySinghSaini/orgs",
-  repos_url: "https://api.github.com/users/SunnySinghSaini/repos",
-  events_url: "https://api.github.com/users/SunnySinghSaini/events{/privacy}",
-  received_events_url: "https://api.github.com/users/SunnySinghSaini/received_events",
-  type: "User",
-  site_admin: false,
-  name: "Sunny",
-  company: null,
-  blog: "",
-  location: null,
-  email: null,
-  hireable: null,
-  bio: null,
-  twitter_username: null,
-  public_repos: 2,
-  public_gists: 0,
-  followers: 0,
-  following: 0,
-  created_at: "2017-09-02T18:44:59Z",
-  updated_at: "2024-06-01T09:14:01Z",
-};
+// Define __dirname and __filename for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+// Define routes
 app.get('/', (req, res) => {
   res.send('<div> <h1>This home page is developed by Sunny !!!</h1> <button> <a href="https://sunny-app.onrender.com/youtube">Youtube </a> </button><br> <br> </div> <div> <button> <a href="https://sunny-app.onrender.com/login">Login </a> </button> </div>');
 });
@@ -55,7 +30,12 @@ app.get('/youtube', (req, res) => {
 });
 
 app.get('/github', (req, res) => {
-  res.json(getGithubData); // Ensure proper response type for JSON data
+  res.json(getGithubData);
+});
+
+// Serve the portfolio.html file
+app.get('/portfolio', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'portfolio.html'));
 });
 
 app.listen(port, () => {
