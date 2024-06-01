91d5f0a180d2a0d173e043c28e6b02a70e6a8e72
Sunny <sun.ss.6767@gmail.com>
2024-06-01 15:50:54 +0530

    github variable added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 require('dotenv').config()
 const app = express()
 const port = 5000
+const getGithubData = "https://api.github.com/users/SunnySinghSaini"
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -12,7 +13,9 @@ app.get('/login', (req,res) => {
 app.get('/youtube', (req,res) => {
     res.send("Here is your Youtube")
 })
-
+app.get('/github', (req,res) => {
+    res.send(getGithubData)
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
