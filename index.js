5a1d46e29559f565036c3eef1f0607bd097e663f
Sunny <sun.ss.6767@gmail.com>
2024-06-01 19:38:41 +0530

    new port method added

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 const getGithubData = {
   login: "SunnySinghSaini",
